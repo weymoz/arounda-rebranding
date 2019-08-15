a6e69a85560480a602037015883e561ea638d5f4
Metprox <olegvakarchuk94@gmail.com>
2019-08-15 19:40:16 +0300

    3 day fixing Pixel Perfect

--- src/components/pages/Main/index.js
+++ src/components/pages/Main/index.js
@@ -1,51 +1,52 @@
-import React, {Component,Fragment} from 'react';
-import {TimelineMax} from 'gsap';
-import WorkSection from "@sections/WorkSection";
-import HeadingSection from "@sections/HeadingSection";
-import WhoSection from "@sections/WhoSection";
-import WhySection from "@sections/WhySection";
-import WhatSection from "@sections/WhatSection";
-import HowSection from "@sections/HowSection";
-import BlogSection from "@sections/BlogSection";
-import StartSection from "@sections/StartSection";
-import PartnerSection from "@sections/PartnerSection";
-import VideoSection from "@sections/VideoSection";
-import BackgroundAnimate from "@simple/BackgroundAnimate";
+import React, { Component, Fragment } from 'react';
+import { TimelineMax } from 'gsap';
+import style from './style.scss';
+import WorkSection from '@sections/WorkSection';
+import HeadingSection from '@sections/HeadingSection';
+import WhoSection from '@sections/WhoSection';
+import WhySection from '@sections/WhySection';
+import WhatSection from '@sections/WhatSection';
+import HowSection from '@sections/HowSection';
+import BlogSection from '@sections/BlogSection';
+import StartSection from '@sections/StartSection';
+import PartnerSection from '@sections/PartnerSection';
+import VideoSection from '@sections/VideoSection';
+import BackgroundAnimate from '@simple/BackgroundAnimate';
 
 class Main extends Component {
-    get tl() {
-		const tl = new TimelineMax();
-		const headingTl = new TimelineMax();
-		const bgTl = new TimelineMax();
-		
+  get tl() {
+    const tl = new TimelineMax();
+    const headingTl = new TimelineMax();
+    const bgTl = new TimelineMax();
 
+    headingTl.add(this.heading.tl);
+    bgTl.add(this.bg.tween);
 
+    tl.add(bgTl, 'start').add(headingTl, '-=.3');
 
-		headingTl.add(this.heading.tl);
-		bgTl.add(this.bg.tween)
+    return tl;
+  }
+  render() {
+    return (
+      <Fragment>
+        <HeadingSection ref={el => (this.heading = el)} />
+        <BackgroundAnimate ref={el => (this.bg = el)} />
+        <VideoSection />
+        <PartnerSection />
+        <section className={style.wrapWorkSection}>
+          <WorkSection />
+        </section>
+        <WhoSection />
+        <WhySection />
+        <WhatSection />
+        <HowSection />
+        <BlogSection />
+        <section className={style.wrapStartSection}>
 
-        tl.add(bgTl, 'start').add(headingTl, '-=.3')
-        
-        return tl;
-	
-    }
-    render() {
-        return (
-            <Fragment>
-                <HeadingSection ref={el => this.heading = el}/>
-                <BackgroundAnimate ref={el => this.bg = el}/>
-                <VideoSection/>
-                <PartnerSection />
-                <WorkSection/>
-                <WhoSection/>
-                <WhySection/>
-                <WhatSection/>
-                <HowSection/>
-                <BlogSection/>
-                <StartSection/>
-            </Fragment>
-        )
-    }
-    
-};
+        <StartSection />
+        </section>
+      </Fragment>
+    );
+  }
+}
 export default Main;
--- src/components/sections/CapabilitySection/index.js
+++ src/components/sections/CapabilitySection/index.js
@@ -3,49 +3,42 @@ import TitleSecondary from '@simple/TitleSecondary';
 import Description from '@simple/Description';
 import style from './style.scss';
 
-const CapabilitySection = (props) => {
-    return (
-        <section className={style.capability}>
-            <div className={style.content}>
-                <div className={style.titleWrapper}>
-                    <TitleSecondary text='Capabilities' />
-                </div>
-                <div className={style.descWrapper}>
-                    <Description light={true} text='A system of actions aimed at identifying a digital product in a digital environment. Creating a variety of graphic media based on the mental shell of the product.' />
-                </div>
-            </div>
-            <div className={style.image}>
-                <img src="assets/images/img-3.png" alt="lego" />
-            </div>
-            <div className={style.list}>
-                <div>
-                    <span className={style.item}>
-                        Strategy
-                            </span>
-                </div>
-                <div>
-                    <span className={style.item}>
-                        UI Design
-                            </span>
-                </div>
-                <div>
-                    <span className={style.item}>
-                        Development
-                            </span>
-                </div>
-                <div>
-                    <span className={style.item}>
-                        UX Design
-                            </span>
-                </div>
-                <div>
-                    <span className={style.item}>
-                        Branding
-                            </span>
-                </div>
-            </div>
-        </section>
-    )
+const CapabilitySection = props => {
+  return (
+    <section className={style.capability}>
+      <div className={style.content}>
+        <div className={style.titleWrapper}>
+          <TitleSecondary text='Capabilities' />
+        </div>
+        <div className={style.descWrapper}>
+          <Description
+            light={true}
+            text='A system of actions aimed at identifying a digital product in a digital environment. Creating a variety of graphic media based on the mental shell of the product.'
+          />
+        </div>
+      </div>
+      <div className={style.image}>
+        <img src='assets/images/img-3.png' alt='lego' />
+      </div>
+      <div className={style.list}>
+        <div>
+          <span className={style.item}>Strategy</span>
+        </div>
+        <div>
+          <span className={style.item}>UX Design</span>
+        </div>
+        <div>
+          <span className={style.item}>UI Design</span>
+        </div>
+        <div>
+          <span className={style.item}>Development</span>
+        </div>
+        <div>
+          <span className={style.item}>Branding</span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default CapabilitySection;
