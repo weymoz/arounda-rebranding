3d2da30186e14cee48ef071d458b74819738350a
Metprox <olegvakarchuk94@gmail.com>
2019-08-27 17:25:50 +0300

    Adaptive 768px - Ready

--- src/components/sections/BlogSection/index.js
+++ src/components/sections/BlogSection/index.js
@@ -3,7 +3,7 @@ import style from './style.scss';
 import Title from '@simple/Title';
 
 const BlogSection = props => {
-  const posts = [
+  const postsDesktop = [
     {
       id: 0,
       date: 'May 17',
@@ -25,6 +25,29 @@ const BlogSection = props => {
       title2: 'from Headspacev'
     }
   ];
+  const postsTablet = [
+    {
+      id: 0,
+      date: 'May 17',
+      category: 'Culture',
+      title1: 'How will technology shape UX',
+      title2: 'Design in 2019'
+    },
+    {
+      id: 1,
+      date: 'May 17',
+      category: 'UX Design',
+      title1: 'Six tips for user testing'
+    },
+    {
+      id: 2,
+      date: 'May 17',
+      category: 'Culture',
+      title1: '6 things we learned about',
+      title2: 'animation from Headspacev'
+    }
+  ];
+
 
   return (
     <section className={style.section}>
@@ -32,8 +55,8 @@ const BlogSection = props => {
         <Title text={'Blog and news'} />
       </div>
       <div className={style.posts}>
-        <ul>
-          {posts.map(el => {
+        <ul className={style.listForDesktop}>
+          {postsDesktop.map(el => {
             return (
               <li key={el.id} className={style.post}>
                 <div className={style.date}>
@@ -45,6 +68,25 @@ const BlogSection = props => {
                   <span>{el.title1}</span>
                   {el.title2 ? <br /> : null}
                   <span>{el.title2}</span>
+                </div>
+                <div className={style.image}>
+                  <img src='./assets/images/img-4.png' alt='image' />
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+        <ul className={style.listForTablet}>
+          {postsTablet.map(el => {
+            return (
+              <li key={el.id} className={style.post}>
+                <div className={style.date}>
+                  <span>{el.date}</span>
+                  <span className={style.dash}>/</span>
+                  <span>{el.category}</span>
+                </div>
+                <div className={style.title}>
+                  <span>{el.title1}</span> <span>{el.title2}</span>
                 </div>
                 <div className={style.image}>
                   <img src='./assets/images/img-4.png' alt='image' />
