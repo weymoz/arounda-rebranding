78ff9e8baa107b853b04c938996c56da79ede89f
Metprox <olegvakarchuk94@gmail.com>
2019-09-16 13:33:32 +0300

    add blog end desktop

--- src/components/pages/BlogItem/index.js
+++ src/components/pages/BlogItem/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import style from './style.scss';
 import blogs from '@/data/BlogItems';
 
@@ -22,8 +22,36 @@ const BlogItem = props => {
               </div>
             </div>
             <div className={style.body}>
-              <span>{blog.caseStudy.title}</span>
-              <span>{blog.what.text}</span>
+              <span className={style.case}>{blog.caseStudy.title}</span>
+              <p className={style.caseText1}>{blog.caseStudy.text1}</p>
+              <ul>
+                {blog.caseStudy.list.map(item => (
+                  <li key={item.id}>{item.text}</li>
+                ))}
+              </ul>
+              <p className={style.caseText2}>{blog.caseStudy.text2}</p>
+              <h2 className={style.whatTitle}>{blog.what.title}</h2>
+              <p className={style.whatText}>{blog.what.text}</p>
+              <p className={style.whatText2}>{blog.what.text2}</p>
+              <ul className={style.touchingList}>
+                {blog.what.list.map(item => (
+                  <li key={item.id}>{item.text}</li>
+                ))}
+              </ul>
+              <p className={style.whatText3}>{blog.what.text3}</p>
+              <p className={style.whatText4}>{blog.what.text4}</p>
+              <p className={style.whatText5}>{blog.what.text5}</p>
+              <img className={style.whatImg} src={blog.what.img} alt={blog.what.imgDescription} />
+              <span className={style.whatImgDescription}>{blog.what.imgDescription}</span>
+              <p className={style.whatText6}>{blog.what.text6}</p>
+              <p className={style.whatText7}>{blog.what.text7}</p>
+              <p className={style.whatText8}>{blog.what.text8}</p>
+              <p className={style.whatText9}>{blog.what.text9}</p>
+              <h2 className={style.whatPreverb}>{blog.what.proverb}</h2>
+              <span className={style.whatPreverbDescription}>{blog.what.proverbDescription}</span>
+              <h2 className={style.conclusionTitle}>{blog.conclusion.title}</h2>
+              <p className={style.conclusionText}>{blog.conclusion.text}</p>
+              <p>{blog.conclusion.text2}</p>
             </div>
           </div>
         ) : null
